ae80b4fac477d09606de8fd918d01853e3dd95d0
pi3 <appmovies2dam>
2016-03-19 22:29:46 +0100

    first commit

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('inicioCtrl', function($scope) {
+
+})
+   
+.controller('estrenosCtrl', function($scope) {
+
+})
+   
+.controller('cinesCercanosCtrl', function($scope) {
+
+})
+      
+.controller('trailersCtrl', function($scope) {
+
+})
+   
+.controller('ayudaCtrl', function($scope) {
+
+})
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,73 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('menu.inicio', {
+    url: '/inicio',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/inicio.html',
+        controller: 'inicioCtrl'
+      }
+    }
+  })
+
+  .state('menu.estrenos', {
+    url: '/estrenos',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/estrenos.html',
+        controller: 'estrenosCtrl'
+      }
+    }
+  })
+
+  .state('menu.cinesCercanos', {
+    url: '/cines',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cinesCercanos.html',
+        controller: 'cinesCercanosCtrl'
+      }
+    }
+  })
+
+  .state('menu', {
+    url: '/side-menu21',
+    templateUrl: 'templates/menu.html',
+    abstract:true
+  })
+
+  .state('menu.trailers', {
+    url: '/trailers',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/trailers.html',
+        controller: 'trailersCtrl'
+      }
+    }
+  })
+
+  .state('menu.ayuda', {
+    url: '/ayuda',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/ayuda.html',
+        controller: 'ayudaCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/inicio')
+
+  
+
+});
